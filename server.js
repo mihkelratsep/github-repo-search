4a514022fa937acb9762b86be7f988addcd6954e
mihkelratsep <mihkel.ratsep@guaana.com>
2020-03-09 14:44:18 +0200

    history

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(history());
+app.use(history({
+  verbose: true,
+}));
+app.use(express.static('.'));
 const port = process.env.PORT || 80;
 app.listen(port);
